a70b4170e224417784e49e2819ec3371897f37d6
Crystal Wang <crystalmaro@gmail.com>
2019-07-14 18:25:58 +0800

    fix color chip rendering. complete week1part3 assignment

--- students/crystal/stylish/index.js
+++ students/crystal/stylish/index.js
@@ -4,14 +4,14 @@ const API_HOST = "https://api.appworks-school.tw/api/1.0";
 // Render & display /products/all on homePage initial loading
 // another method: window.addEventListener()
 getProductCategory(API_HOST+"/products/all", function(response) {
-    render(response);
-    });
+  render(response);
+});
 
 // Remove existing elements when loading new category
 function removeElement(className){
   let elements = document.querySelector("."+className);
     while(elements.firstChild){
-        elements.removeChild(elements.firstChild);
+      elements.removeChild(elements.firstChild);
     }
 }
 
@@ -26,13 +26,13 @@ const getProducts = (type) => {
 function getProductCategory(src, callback) { 
   let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            callback(xhr.response);
-        }
-    }
-    xhr.open("GET", src);
-    xhr.send();
- }
+      if (this.readyState === 4 && this.status === 200) {
+        callback(xhr.response);
+      }
+}
+  xhr.open("GET", src);
+  xhr.send();
+}
 
  // Product display rendering dynamically
 function render(data) {
@@ -44,45 +44,47 @@ function render(data) {
       let product_container = document.createElement("div");
         product_container.setAttribute("class", "product_container");
     
-        // div.product_main_img
+      // div.product_main_img
       let product_main_img = document.createElement("img")
-        product_main_img.setAttribute("class", "product_main_img")
-        product_main_img.setAttribute("src", `${product[i].main_image}`);
-        product_container.appendChild(product_main_img);
+      product_main_img.setAttribute("class", "product_main_img")
+      product_main_img.setAttribute("src", `${product[i].main_image}`);
+      product_container.appendChild(product_main_img);
 
-        // div.all_colors
+      // div.all_colors
       let all_colors = document.createElement("div");
         all_colors.setAttribute("class", "all_colors");
-
-        
-
+      product_container.appendChild(all_colors);
         // div.all_colors (individual color chip)
+      product[i].colors.forEach(color => {
+        colorChip = document.createElement("div");
+        colorChip.setAttribute("class", "color");
+        colorChip.setAttribute("style", "background-color:#" + color.code);
+        colorChip.title = color.name;
+        all_colors.appendChild(colorChip);
+      })
+        /* below for loop error:
+        create color chips for all products using first item's color
         for (let i = 0; i < product[i].colors.length; i++) { 
-
-            // removeElement("all_colors");
-
             let color = document.createElement("div");
             color.setAttribute("class", "color");
             color.setAttribute("style", `background-color:#${product[i].colors[i].code};`);
-            // @TODO need to append corresponding color child to color containers
-            // now all products are all using the first product's colors TT___TT
             all_colors.appendChild(color);
-        }
-
-        product_container.appendChild(all_colors);
+        } */
       
         // div.product_name
       let product_name = document.createElement("div");
-        product_name.setAttribute("class", "product_name");
-        product_name.innerHTML = product[i].title;
-        product_container.appendChild(product_name);
+      product_name.setAttribute("class", "product_name");
+      product_name.innerHTML = product[i].title;
+      product_container.appendChild(product_name);
 
         // div.product_price
       let product_price = document.createElement("div");
-        product_price.setAttribute("class", "product_price");
-        product_price.appendChild(document.createTextNode(`TWD.${product[i].price}`))
-        product_container.appendChild(product_price);
+      product_price.setAttribute("class", "product_price");
+      product_price.appendChild(document.createTextNode(`TWD.${product[i].price}`))
+      product_container.appendChild(product_price);
 
-        all_products.appendChild(product_container);
+      all_products.appendChild(product_container);
     }
 }
+
+
