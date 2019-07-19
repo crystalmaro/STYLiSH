1a33c82651c81b9c74ad45d24c3ec6d5a0487c92
Crystal Wang <crystalmaro@gmail.com>
2019-07-19 21:29:09 +0800

    wip add to cart button, dynamic size classlist according to color+size stock

--- students/crystal/stylish/js/product.js
+++ students/crystal/stylish/js/product.js
@@ -25,7 +25,6 @@ function renderItem (data) {
   let itemContainer = document.querySelector(".itemContainer");
   let item = data.data;
   variants = item.variants;
-  console.log(variants.length)
 
   // A) itemMainImg (upper left section)
   let itemMainImg = document.querySelector(".itemMainImg");
@@ -33,18 +32,18 @@ function renderItem (data) {
   mainImg.setAttribute("src", item.main_image);
   itemMainImg.appendChild(mainImg);
 
- // B) itemDetails (upper right section)
+  // B) itemDetails (upper right section)
   document.querySelector(".itemName").textContent = item.title;
   document.querySelector(".itemID").textContent = item.id;
   document.querySelector(".itemPrice").textContent = `TWD. ${item.price}`;
   // item color loop
   let itemColors = document.querySelector(".itemColors");
   for (let i = 0; i < item.colors.length; i++) {
-
     let itemColorChip = document.createElement("div");
     itemColorChip.className = "itemColorChip";
     itemColorChip.setAttribute("style", `background-color:#${item.colors[i].code}`);
     itemColorChip.setAttribute("onClick", `selectedColor(${i})`);
+    // loop color_code to #id to assign to global variable currentColorID
     itemColorChip.setAttribute("id", item.colors[i].code);
     itemColors.appendChild(itemColorChip);
     switch(i) {
@@ -52,9 +51,8 @@ function renderItem (data) {
         itemColorChip.classList.add("current");
         break;
     }
-    currentColorID = document.querySelectorAll(".current")[0].id;
-    // console.log(currentColorID);
   };
+  currentColorID = document.querySelectorAll(".current")[0].id;
   // item size loop
   let itemSizes = document.querySelector(".itemSizes");
   for (let i = 0; i < item.sizes.length; i++) {
@@ -62,6 +60,7 @@ function renderItem (data) {
     sizeCircle.className = "sizeCircle";
     sizeCircle.innerHTML = item.sizes[i];
     sizeCircle.setAttribute("onClick", `selectedSize(${i})`)
+    // loop size to #id to assign to global variable currentColorID
     sizeCircle.setAttribute("id", item.sizes[i])
     itemSizes.appendChild(sizeCircle);
     switch(i) {
@@ -69,16 +68,15 @@ function renderItem (data) {
         sizeCircle.classList.add("current");
         break;
     }
-    currentSizeID = document.querySelectorAll(".current")[1].id;
-    // console.log(currentSizeID);
   };
+  currentSizeID = document.querySelectorAll(".current")[1].id;
   document.querySelector(".itemNote").innerHTML = `*${item.note}`;
   document.querySelector(".itemTexture").innerHTML = item.texture;
   document.querySelector(".itemDesc").innerHTML = item.description.replace(/\r\n/g, "<br/>");;
   document.querySelector(".itemWash").innerHTML = `清洗：${item.wash}`;
   document.querySelector(".itemPlace").innerHTML = `產地：${item.place}`;
    
-  // @todo C) itemInfo (additional item images)
+  // C) itemInfo (story & additional item images)
   document.querySelector(".itemInfoStory").innerHTML = item.story;
   let itemInfoImg = document.querySelector(".itemInfoImg");
   item.images.forEach(img => {
@@ -88,72 +86,24 @@ function renderItem (data) {
   });
 };
 
-
-
-
 /* ==================
 onClick() CSS for selected color and size
 ================== */
 const selectedColor = (index) => {
 let itemColorChip = document.getElementsByClassName("itemColorChip");
+let sizeCircle = document.getElementsByClassName("sizeCircle");
   for (let i = 0; i < 3; i++) {
+    // 砍掉重練 reset every color chip
     itemColorChip[i].classList.remove("current");
-    if (index !== undefined) {
+    if (true) {
       itemColorChip[index].classList.add("current");
       currentColorID = itemColorChip[index].id;
-        if ()
-    } 
-    // else {
-    //   itemColorChip[index].classList.remove("current");
-    // }
+    }
   }
-  checkStock();
+  // check and update stock on each click
+  fetchStock();
   console.log(currentColorID);
 };
-
-function checkStock () {
-  const stockArray = variants.filter(stock => 
-    (currentColorID === stock.color_code && 
-    currentSizeID === stock.size && 
-    stock.stock > 0));
-
-  if (stockArray.length > 0) {
-    currentStock = stockArray[0].stock
-  } else {
-    currentStock = 0;
-  }
-}
-
-// function selectedColor (index) {
-//   let itemColorChip = document.getElementsByClassName("itemColorChip");
-//   // for (let i = 0; i < 3; i++) {
-    
-//     // if (index !== undefined) {
-//     //   itemColorChip[index].classList.add("current");
-//     // } else {
-//     //   itemColorChip[index].classList.add("remove");
-//     // }
-
-//     for (let i = 0; i < variants.length; i++) {
-//     if (currentColorID === variants[i].color_code && currentSizeID === variants[i].size) {
-//       itemColorChip[index].classList.add("current");
-//       currentColorID = itemColorChip[index].id;
-//       currentStock = variants[i].stock;  
-//     }
-//   }
-//   // }
-//   console.log("currentColor: "+currentColorID)
-//   console.log("currentSize: "+currentSizeID)
-//   console.log(variants)
-//   console.log("current stock: "+currentStock)
-// }
-
-// function selectedSize (index) {
-//   let sizeCircle = document.getElementsByClassName("sizeCircle");
-//   for (let i = 0; i < variants.length; i++) {
-//     i
-//   }
-// }
 
 const selectedSize = (index) => {
   let sizeCircle = document.getElementsByClassName("sizeCircle");
@@ -163,25 +113,57 @@ const selectedSize = (index) => {
       sizeCircle[index].classList.add("current");
       currentSizeID = sizeCircle[index].id;
     } 
-    else {
-      sizeCircle[index].classList.remove("current");
-    }
   }
-  checkStock();
+  // check and update stock on each click
+  fetchStock();
   console.log(currentSizeID);
 };
 
+function fetchStock() {
+  let stockArray = variants.filter(item => 
+    (currentColorID === item.color_code && 
+    currentSizeID === item.size && 
+    item.stock > 0));
+
+  if (stockArray.length > 0) {
+    currentStock = stockArray[0].stock
+  } else {
+
+    currentStock = 0;
+  }
+  console.log(stockArray)
+}
+
+function checkStock() {
+
+  // @todo check current color's size's stock
+  // if variants.stock = 0, then classList.add("disabled")
+
+}
 
 
 /* ==================
 Button: Add to Cart
 ================== */
-// let cartMinus = document.querySelectorAll(".qtyModify")[0];
-// let cartPlus = document.querySelectorAll(".qtyModify")[1];
-// let qtyValue = document.querySelector(".qtyValue");
-// let initValue = 1;
-// let maxQty = false;
+let cartMinus = document.querySelectorAll(".qtyModify")[0];
+let cartPlus = document.querySelectorAll(".qtyModify")[1];
+let qtyValue = document.querySelector(".qtyValue");
+let initValue = 1;
 
-// cartPlus.addEventListener("click", function(){
-//   qtyValue.textContent = x++
-// });
+function qtyBtn (action) {
+let qtyValue = document.querySelector(".qtyValue").textContent;
+let parsedQty = parseInt(qtyValue) //change string to number
+  if (action === "add") {
+    if (parsedQty < currentStock) {
+      parsedQty++;
+      qtyValue.textContent = parsedQty;
+    } 
+  }
+    
+  if (action === "minus") {
+    if (parsedQty > 1) {
+      parsedQty-- 
+    }
+    qtyValue.textContent = parsedQty
+  }
+};
