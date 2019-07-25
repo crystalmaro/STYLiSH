d840dd0621697bfdb8e9bae9866c423e11e75ded
Crystal Wang <crystalmaro@gmail.com>
2019-07-25 16:22:24 +0800

    adjust css styling for TapPay input fields

--- students/crystal/stylish/js/tapPay.js
+++ students/crystal/stylish/js/tapPay.js
@@ -4,13 +4,12 @@
 // 設置好等等 GetPrime 所需要的金鑰
 let tap = {
     APP_ID: 12348,
-    APP_KEY: app_pa1pQcKoY22IlnSXq5m5WP5jFKzoRG58VEXpT7wU62ud7mMbDOGzCYIlzzLF,
+    APP_KEY: "app_pa1pQcKoY22IlnSXq5m5WP5jFKzoRG58VEXpT7wU62ud7mMbDOGzCYIlzzLF",
     serverType: "sandbox"
 };
 TPDirect.setupSDK(tap.APP_ID, tap.APP_KEY, tap.serverType);
 
 // 把 TapPay 內建輸入卡號的表單給植入到 div 中
-// TPDirect.card.setup(config);
 TPDirect.card.setup({
     fields: {
         number: {
@@ -68,58 +67,56 @@ TPDirect.card.setup({
 });
 
 // ======================= onUpdate
-// TPDirect.card.onUpdate(callback);
 
-TPDirect.card.onUpdate(function (update) {
-    // update.canGetPrime === true
-    // --> you can call TPDirect.card.getPrime()
-    if (update.canGetPrime) {
-        // Enable submit Button to get prime.
-        // submitButton.removeAttribute('disabled')
-    } else {
-        // Disable submit Button to get prime.
-        // submitButton.setAttribute('disabled', true)
-    }
+// TPDirect.card.onUpdate(function (update) {
+//     // update.canGetPrime === true
+//     // --> you can call TPDirect.card.getPrime()
+//     if (update.canGetPrime) {
+//         // Enable submit Button to get prime.
+//         // submitButton.removeAttribute('disabled')
+//     } else {
+//         // Disable submit Button to get prime.
+//         // submitButton.setAttribute('disabled', true)
+//     }
 
-    // cardTypes = ['mastercard', 'visa', 'jcb', 'amex', 'unknown']
-    if (update.cardType === 'visa') {
-        // Handle card type visa.
-    }
+//     // cardTypes = ['mastercard', 'visa', 'jcb', 'amex', 'unknown']
+//     if (update.cardType === 'visa') {
+//         // Handle card type visa.
+//     }
 
-    // number fields is error
-    if (update.status.number === 2) {
-        // setNumberFormGroupToError()
-    } else if (update.status.number === 0) {
-        // setNumberFormGroupToSuccess()
-    } else {
-        // setNumberFormGroupToNormal()
-    }
+//     // number fields is error
+//     if (update.status.number === 2) {
+//         // setNumberFormGroupToError()
+//     } else if (update.status.number === 0) {
+//         // setNumberFormGroupToSuccess()
+//     } else {
+//         // setNumberFormGroupToNormal()
+//     }
 
-    if (update.status.expiry === 2) {
-        // setNumberFormGroupToError()
-    } else if (update.status.expiry === 0) {
-        // setNumberFormGroupToSuccess()
-    } else {
-        // setNumberFormGroupToNormal()
-    }
+//     if (update.status.expiry === 2) {
+//         // setNumberFormGroupToError()
+//     } else if (update.status.expiry === 0) {
+//         // setNumberFormGroupToSuccess()
+//     } else {
+//         // setNumberFormGroupToNormal()
+//     }
 
-    if (update.status.cvc === 2) {
-        // setNumberFormGroupToError()
-    } else if (update.status.cvc === 0) {
-        // setNumberFormGroupToSuccess()
-    } else {
-        // setNumberFormGroupToNormal()
-    }
-});
+//     if (update.status.cvc === 2) {
+//         // setNumberFormGroupToError()
+//     } else if (update.status.cvc === 0) {
+//         // setNumberFormGroupToSuccess()
+//     } else {
+//         // setNumberFormGroupToNormal()
+//     }
+// });
 
 // ======================= getTappay Fields Status
 TPDirect.card.getTappayFieldsStatus();
 
 // ======================= get Prime
 // call TPDirect.card.getPrime when user submit form to get tappay prime
-TPDirect.card.getPrime(callback);
 
-let submitButton = document.querySelector("#submitButton");
+// let submitButton = document.querySelector("#submitButton");
 
 function getPrime(){
     event.preventDefault();
@@ -148,16 +145,15 @@ function getPrime(){
 };
 
 // ======================================================================
-let submitButton = document.querySelector('#submit-button');
 
-function onClick() {
-  // 讓 button click 之後觸發 getPrime 方法
-  TPDirect.card.getPrime(function (result) {
-    if (result.status !== 0) {
-      console.err('getPrime 錯誤')
-      return
-    }
-    let prime = result.card.prime
-    alert('getPrime 成功: ' + prime)
-  });
-};
+// function onClick() {
+//   // 讓 button click 之後觸發 getPrime 方法
+//   TPDirect.card.getPrime(function (result) {
+//     if (result.status !== 0) {
+//       console.err('getPrime 錯誤')
+//       return
+//     }
+//     let prime = result.card.prime
+//     alert('getPrime 成功: ' + prime)
+//   });
+// };
